1aed438ac3d42ca4263260817beaa450d2a95eed
Abdur Rahman <akaid.dev@gmail.com>
2022-01-25 20:09:09 +0600

    add update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import "./App.css";
-import logo from "./logo.svg";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h2>Akaid.dev</h2>
-        <p> I'm coming soon </p>
+    <div>
+      {/* Header */}
+      <header className='w3-container w3-red w3-center' style={{ padding: "142px 16px" }}>
+        <h1 className='w3-margin w3-jumbo'> AKAID.DEV </h1>
+        <p className='w3-xlarge'>I am coming soon </p>
+        <button className='w3-button w3-black w3-margin-top'>Get In Touch</button>
       </header>
+
+      <div className='w3-container w3-blue w3-center w3-opacity w3-padding-16'>
+        <h1 className='w3-margin w3-xlarge'>Abdur Rahman</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
